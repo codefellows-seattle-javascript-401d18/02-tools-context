9986a5fb326eddbee2f8a34f32b5f6905f56be09
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-08-22 16:55:10 -0700

    Ok the test is actually failing now instead of just teh suite failing

--- lab-michelle/__test__/fp.test.js
+++ lab-michelle/__test__/fp.test.js
@@ -5,7 +5,7 @@ const fp = require('../lib/fp');
 describe('Testing fp.js', function () {
   describe('default properties', () => {
     test('Should fail because this is TDD', () => {
-      expect(map([1,2,3], num=>num*2)).toEqual([3, 6, 9]);
+      expect(fp.map([1,2,3], num=>num*2)).toEqual([3, 6, 9]);
     });
   });
 });
