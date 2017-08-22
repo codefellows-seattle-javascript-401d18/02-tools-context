06fd798503fae5d82387f7b8391c275183a8e633
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-08-22 15:12:27 -0700

    Some scaffolding

---
+++ lab-michelle/__test__/fp.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const fp = require('../lib/fp');
+
+describe('Testing fp.js', function () {
+  describe('default properties', () => {
+    test('Should fail because this is TDD', () => {
+      expect(something).toEqual(something);
+    });
+  });
+});
