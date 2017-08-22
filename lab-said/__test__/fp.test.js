32438115d34264099b8e097ab4a3b1edaca10433
saidmattar <ssabdou@gmail.com>
2017-08-22 16:07:02 -0700

    tested a filter function

--- lab-said/__test__/fp.test.js
+++ lab-said/__test__/fp.test.js
@@ -6,5 +6,8 @@ describe('fp.js', function() {
     test('should return', ()=> {
       expect(fp.myMap([4,6,8],(num => num * 2))).toEqual([8,12,16]);
     });
+    test('should return', ()=> {
+      expect(fp.myFilter([12,14,17,18,20],(num => num >= 18))).toEqual([18,20]);
+    });
   });
 });
--- lab-said/lib/fp.js
+++ lab-said/lib/fp.js
@@ -10,3 +10,15 @@ arrMethod.myMap = function(arr, callback) {
 arrMethod.myFilter = function(arr, callback) {
   return Array.prototype.filter.call(arr, callback);
 };
+
+// arrMethod.myReduce = function(arr, callback) {
+//   return Array.prototype.reduce.call(arr, callback);
+// };
+//
+// arrMethod.myConcatr = function(arr, callback) {
+//   return Array.prototype.concat.call(arr, callback);
+// };
+//
+// arrMethod.mySplice = function(arr, callback) {
+//   return Array.prototype.splice.call(arr, callback);
+// };
