3f8ad7e65a6059377a4dfcf3ce1e1e2fd3553043
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-22 14:04:47 -0700

    test a written, working out kinks

--- shaun-lab02/__tests__/fp-test.js
+++ shaun-lab02/__tests__/fp-test.js
@@ -11,20 +11,37 @@ describe('frontPage', () => {
       expect(fp.map).toEqual([2, 4, 6, 8]);
     });
   });
-describe('#filter', ()=>{
-  it('should return [1, 2, 3]', ()=> {
-    expect(fp.filter).toEqual([1, 2, 3]);
+  describe('#filter', ()=>{
+    it('should return [1, 2, 3]', ()=> {
+      var result = fp.filter([1, 2, 3, 4], n => n !== 2);
+      expect(fp.filter).toEqual([1, 2, 3]);
+    });
   });
-});
-describe('#concat', () => {
-  it('should return [1, 2, 3, a, b, c]', () =>{
-    except(fp.concat)
-    expect(result).toEqual([1, 2, 3, a, b, c]);
+  describe('#concat', () => {
+    it('should return [1, 2, 3, a, b, c]', () =>{
+      var result = fp.concat([1, 2, 3], ['a', 'b', 'c']);
+      except(fp.concat).toEqual([1, 2, 3, 'a', 'b', 'c']);
+    })
   })
-})
-});
-// describe('#reduce', () => {
-//   it('should return')
-// })
-// describe('#splice')
-// });
+describe('#splice', () => {
+   it('should return [duck, duck, duck]', () => {
+     var result = fp.splice(['duck', 'duck', 'goose', 'duck'], 2, 1);
+     expect(fp.splice).toEqual(['duck', 'duck', 'duck']);
+   });
+
+   it('should return [duck0, duck1, oyster, duck2]', () => {
+     var result = fp.splice(['duck0', 'duck1', 'duck2'], 2, 0, ['oyster']);
+     expect(fp.splice).toEqual(['duck0', 'duck1', 'oyster', 'duck2']);
+   });
+ });
+ describe('#reduce', () => {
+   it('should return 25', () => {
+     var result = fp.reduce([1, 2, 3, 4], (a, b) => a + b, 15);
+     expect(fp.reduce).to.equal(25);
+   });
+
+   it('should return 10', () => {
+     var result = fp.reduce([1, 2, 3, 4], (a, b) => a + b);
+     expect(fp.reduce).to.equal(10);
+   });
+ });
--- shaun-lab02/lib/fp.js
+++ shaun-lab02/lib/fp.js
@@ -23,11 +23,14 @@ exports.concat = (arr1, arr2) => {
   return Array.prototype.concat.apply(arr1, arr2);
 };
 
-exports.map = (arr, callback) => {
-  if(!arr) throw new Error('no array provided');
-  if(!callback) throw new Error('no callback provided');
+exports.splice = (arr, start, deleteCount, item=[]) => {
+  if (!arr) throw new Error('no array provided');
+  if (!start) throw new Error('no start provided');
+  if (!item) throw new Error('item is undefined');
+  if (typeof deleteCount !== 'number') throw new Error('no deleteCount provided');
 
-  return Array.prototype.map.call(arr, callback);
+  return Array.prototype.splice.call(arr, start, deleteCount, ...item;
+  return arr
 };
 
 exports.reduce = (arr, callback, start=0) => {
