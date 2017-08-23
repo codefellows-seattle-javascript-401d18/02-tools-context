608caf0c2c67031f9323622c810024fe4b72e85c
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-08-22 21:09:46 -0700

    I might be done

---
+++ lab-michelle/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const fp = require('./lib/fp');
+console.log(fp);
+console.log(process); //RE bonus points, process has got 9 million things in it, ain't nobody got time for that after this lab!//
